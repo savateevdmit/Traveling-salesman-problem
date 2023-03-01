81035a42811cb9164660ba10c1afc1cf4ed2e3eb
SavD <73773381+savateevdmit@users.noreply.github.com>
2023-03-01 09:09:03 +0300

    Add files via upload
    Обновлены файлы

--- y.py
+++ y.py
@@ -2,7 +2,7 @@ y_input = """
 MDTextField:
     hint_text: "Координата Y"
     helper_text_mode: "on_focus"
-    icon_right: "egg-fried"
+    icon_right: "graphql"
     icon_right_color: app.theme_cls.primary_color
     pos_hint:{'center_x': 0.25, 'center_y': 0.7}
     size_hint_x:None
